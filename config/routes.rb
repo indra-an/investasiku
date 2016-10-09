e0665f5c79a2d049cafd12b7081a90dcc7cc99b5
Rifqi Fauzi <rifqi@Rifqis-MacBook-Pro.local>
2016-10-09 20:39:20 +0700

    fix ckeditor clash with turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   namespace :admins do
     get 'dashboard' => 'dashboard#index'
 
-    put 'update_full_name'
-    put 'update_password'
-    post 'queue_newsletter'
+    put 'update_full_name' => 'dashboard#update_full_name'
+    put 'update_password' => 'dashboard#update_password'
+    post 'queue_newsletter' => 'dashboard#queue_newsletter'
 
     resources :admins, :except => [:show, :edit]
   end
