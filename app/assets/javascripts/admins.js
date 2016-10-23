7c04434b50abe4870b638d056fedd52587376ec3
Rifqi Fauzi <rifqi@Rifqis-MacBook-Pro.local>
2016-10-23 18:55:50 +0700

    initialize ckeditor and chosen jquery at turbolinks callback

--- app/assets/javascripts/admins.js
+++ app/assets/javascripts/admins.js
@@ -6,14 +6,21 @@
 //= require turbolinks
 
 $(document).on('turbolinks:load', function() {
-  // reinitialize ckeditor
-  $('.ckeditor').each(function() {
-    CKEDITOR.replace($(this).attr('id'));
-  });
-
   // reattach callback
   $(".notification").fadeTo(3000, 500).slideUp(500, function() {
       $(".notification").slideUp(500);
+  });
+
+  // reattach chosen jquery
+  $('.chosen').chosen({
+    width: '100%',
+    no_results_text: 'No tags matches',
+    placeholder_text_multiple: 'Select tags'
+  });
+
+  // reinitialize ckeditor
+  $('.ckeditor').each(function() {
+    CKEDITOR.replace($(this).attr('id'));
   });
 });
 
