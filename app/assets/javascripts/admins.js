65e09631b9184cc5065be018b5435ae0c6b0bee4
Rifqi Fauzi <rifqi@Rifqis-MacBook-Pro.local>
2016-10-23 18:47:19 +0700

    alert notification

--- app/assets/javascripts/admins.js
+++ app/assets/javascripts/admins.js
@@ -4,3 +4,16 @@
 //= require ahoy
 //= require ckeditor/init
 //= require turbolinks
+
+$(document).on('turbolinks:load', function() {
+  // reinitialize ckeditor
+  $('.ckeditor').each(function() {
+    CKEDITOR.replace($(this).attr('id'));
+  });
+
+  // reattach callback
+  $(".notification").fadeTo(3000, 500).slideUp(500, function() {
+      $(".notification").slideUp(500);
+  });
+});
+
