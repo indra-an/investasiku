f071869aca2aa25d564f105e345a2f4e28a53f73
Rifqi Fauzi <rifqi@Rifqis-MacBook-Pro.local>
2016-10-31 09:07:03 +0700

    fix groupdate gem cannot use :local timezone

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,5 @@ module Investasiku
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Jakarta'
-    config.active_record.default_timezone = :local
   end
 end
