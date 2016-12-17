b6b290ed7a38355da6b13ab83171afd26d2cccd2
Rifqi Fauzi <rifqi@Rifqis-MacBook-Pro.local>
2016-12-17 16:48:34 +0700

    meeting update

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -6,10 +6,10 @@ class Preference < ApplicationRecord
   belongs_to :module_step_4, :class_name => 'LearningModule'
   belongs_to :module_step_5, :class_name => 'LearningModule'
 
-  validates_presence_of :facebook_link, :twitter_link, :instagram_link, :banner_ad_script,
-                        :sidebar_ad_script, :news_id, :module_step_1_id, :module_step_2_id,
+  validates_presence_of :news_id, :module_step_1_id, :module_step_2_id,
                         :module_step_3_id, :module_step_4_id, :module_step_5_id
-  validates_format_of :facebook_link, :twitter_link, :instagram_link, :with => URI::regexp
+  validates_format_of :facebook_link, :twitter_link, :instagram_link,
+                      :with => URI::regexp, :allow_blank => true
 
   def self.fetch
     first
