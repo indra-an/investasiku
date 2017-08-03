90e895a31d3a0209fb10ac3383453c09c5d3bb44
Indra <indra.abdullah99@gmail.com>
2017-08-03 22:42:49 +0700

    change receipt mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,7 +6,7 @@ class ApplicationMailer < ActionMailer::Base
     @name = params[:name]
     @email = params[:email]
     @content = params[:message].class.eql?(Array) ? params[:message].first : params[:message]
-    mail(to: 'web.investasiku@gmail.com', subject: '[New Contact] ' + @name)
+    mail(to: Rails.application.secrets.admin_email, subject: '[New Contact] ' + @name)
   end
 
   def newsletter(contact, newsletter)
